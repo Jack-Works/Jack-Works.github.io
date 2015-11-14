9089fc36c97ec7df230a70f51ad3d94efe895781
Jack Works <zjwpeter@gmail.com>
2015-11-14 20:55:06 +0800

    Data Update

--- data.js
+++ data.js
@@ -6,7 +6,8 @@
 	}
 	[gen("red"), gen("green"), gen("orange"), gen("gray"), gen("del"),
 		["processing", function (){return this + "...";}],
-		["done", function (){return "✓ " + this;}]
+		["done", function (){return "✓ " + this;}],
+		["right", function (){return "<span class='right'>" + this + "</span>"}]
 	].forEach(function (value){
 		String.prototype.__defineGetter__(value[0], value[1]);
 	});
@@ -15,11 +16,12 @@
 })();
 var conf = {
 	article_title: "Jack Works's Todo list",
-	items: [{title: "哥德尔艾舍尔巴赫",progress: [761, 982, 982], group: "books"},{
-		title: "失控".link("https://github.com/programthink/books/blob/master/README.wiki#107-其它"), progress: [284, 1027, 1027], group: "books"},{
-		title: "Borland 传奇".link("https://github.com/programthink/books/blob/master/README.wiki#106-it-公司"), progress: [246, 507, 507], group: "books"},{
+	items: [{title: "哥德尔艾舍尔巴赫",progress: [764, 982, 982], group: "books"},{
+		title: "失控".link("https://github.com/programthink/books/blob/master/README.wiki#107-其它"), progress: [323, 1027, 1027], group: "books"},{
+		title: "Borland 传奇".link("https://github.com/programthink/books/blob/master/README.wiki#106-it-公司"), progress: [275, 507, 507], group: "books"},{
 		title: "暗时间".link("https://github.com/programthink/books/blob/master/README.wiki#107-其它"), progress: [91, 174, 179], group: "books"},{
-		title: "猜想与反驳".link("https://github.com/programthink/books/blob/master/README.wiki#88-科学哲学"), progress: [9, 62, 722], group: "books"
+		title: "猜想与反驳".link("https://github.com/programthink/books/blob/master/README.wiki#88-科学哲学"), progress: [10, 62, 722], group: "books"},{
+		title: "Doom 启示录", progress: [7, 30, 433], group: "books"
 		},{
 		title: "魔戒",
 		content: ["霍比特人".done.green,
@@ -28,14 +30,6 @@ var conf = {
 			"精灵宝钻", 
 		],
 		progress: [204, 706, 2004],
-		group: "books_details"
-	}, {
-		"title": "三体".id("threebody"),
-		content: ["三体".done.green,
-			"三体 - 黑暗森林".done.green,
-			"三体 - 死神永生".processing.orange
-		],
-		progress: [1528, 1742, 1742],
 		group: "books_details"
 	},{
 		title: "网易云课堂 - 计算机网络之网尽其用".link("http://mooc.study.163.com/course/HIT-1000002010?tid=1000003008"),
@@ -46,31 +40,36 @@ var conf = {
 		progress: [11, 17, 39],
 		group: "netease"
 	},{
-		title: "edX - 60240013x 组合数学".link("https://courses.edx.org/courses/course-v1:TsinghuaX+60240013x+3T2015").id("comb"),
-		content: "当前进度：Different Kinds of Combinations",
-		group: "edX"
-	},{
-		title: "edX - FP101 函数式编程入门".link("https://courses.edx.org/courses/course-v1:DelftX+FP101x+3T2015"),
-		content: "当前进度：Types and Classes",
-		group: "edX"
-	},{
-		title: "edX - DEV204x C# 编程".link("https://courses.edx.org/courses/course-v1:Microsoft+DEV204x+1T2016"),
-		content: "当前进度：Introducing C#",
-		group: "edX"
-	},{
-		title: "edX - PH525.1x 生命科学数据分析 1".link("https://courses.edx.org/courses/course-v1:HarvardX+PH525.1x+3T-2015"),
-		content: "当前进度：Getting Started",
-		group: "edX"
+		title: "edX 课程",
+		content: [
+			"LFS101x.2"        + "Introduction/ Learning Objectives".green.right,
+			"FP101"            + "Defining Functions".green.right,
+			
+			"60240013x"        + "Different Kinds of Combinations".orange.right,
+			"DEV204x"          + "Introducing C#".orange.right,
+			
+			"PH525.1x"         + "Getting Started".gray.right,
+			"HTML5.1x"         + "Introduce Yourself".gray.right,
+			"6.004.1x"         + "Welcome to 6.004.1x!".gray.right,
+			"Think101x"        + "A taste of things to come".gray.right,
+			"Louv1.2x"         + "Introduction to Louv1.2x".gray.right,
+			"CloudIntro.x"     + "Welcome from the Instructor".gray.right,
+			
+			"HTML5.2x"         + "尚未开始".red.right,
+			"6.004.2x"         + "尚未开始".red.right,
+			"IELTSx"           + "尚未开始".red.right,
+			"IT.1.1x"          + "尚未开始".red.right,
+		]
 	},{
 		title: "网易云课堂 - CS 大一".link("http://study.163.com/curricula/cs/grade-1.htm"),
 		content: [
 			"程序设计入门 - Python",
-			"程序设计入门 - C 语言 > " + "C 语言程序设计进阶".gray,
-			("程序设计入门 - Java".red + " > " + "Java 语言程序设计进阶".gray).del,
+			"程序设计入门 - C 语言 > " + " C 语言程序设计进阶".gray,
+			"程序设计入门 - Java > " + " Java 语言程序设计进阶".gray,
 			"计算机专业导论之思维与系统",
 			"计算机专业导论之语言与算法",
 			"计算机专业导论之学科与专业",
-			"工科数学分析(1) > " + "工科数学分析(2)".gray
+			"工科数学分析(1) > " + " 工科数学分析(2)".gray
 		],
 		progress: [0, 7, 10]
 	},{
@@ -96,7 +95,7 @@ var conf = {
 			"数值分析",
 			"概率统计",
 			"复变函数",
-			"高等数学".red.processing,
+			"高等数学",
 			"离散数学".link("#comb").processing,
 			"抽象代數",
 			"拓扑",
@@ -111,17 +110,7 @@ var conf = {
 			"Esperanto",
 			"Latin"
 		]
-	}, /*{ 
-		//Not now, maybe never
-		 title: "物理",
-		content: [
-			"数学物理方程",
-			"量子力学",
-			"统计力学",
-			"经典力学",
-			"电动力学"
-		]
-	}*/, {
+	}, {
 		title: "计算机语言",
 		content: [
 			"JavaScript".done.green,
@@ -188,7 +177,9 @@ var conf = {
 			"提问的智慧".done.green.link("#book_smart_questions") + " 2015/??/??",
 			"黑客与画家".done.green.link("#book_hackers_and_painters") + " 2015/??/??",
 			"Uno".done.green + " 2015/08/18",
-			"我们的征途，是星辰大海！ - Bilibili 专题".done.green.link("http://www.bilibili.com/topic/245.html") + " 2015/10/4"
+			"我们的征途，是星辰大海！ - Bilibili 专题".done.green.link("http://www.bilibili.com/topic/245.html") + " 2015/10/4",
+			"平面国".done.green + " - 2015/11/08",
+			"三体".done.green + " - 2015/11/09"
 		]
 	}]
 }
