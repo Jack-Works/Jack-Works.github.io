c89e9b3ec69dbef624935b08f271786693dd34a0
Jack Works <zjwpeter@gmail.com>
2015-12-15 20:23:52 +0800

    A Thusday Update

--- data.js
+++ data.js
@@ -1,6 +1,11 @@
+/* Hey! 我注释了我暂时还没有开始进行的内容，而不是删除它，更没有修改commit记录
+ * 这是不是能让我看起来不那么好高骛远了？（笑
+ * 好事情要放到开始了再展示，我猜你也是这么想的——如果真的有人会看的话
+ */
 var conf = {
 	article_title: "Jack Works's Todo list",
-	weekMsg: "……哈?我对普通的人类没有兴趣;本周特别更新:;哈利波特与理性之道笔记;".replace(/;/g, "<br>"),
+	weekMsg: "……It's gonna totally awesome!".replace(/;/g, "<br>"),
+	//from 'a very potter musical', awesome!
 	items: [{title: "哥德尔艾舍尔巴赫".id("book_geb") + "人工智能: 回顾 - 改变问题空间".comment, progress: [804, 982], group: "books"},{
 			title: "失控".link("https://github.com/programthink/books/blob/master/README.wiki#107-其它"), progress: [404, 1027], group: "books"},{
 			title: "Borland 传奇".link("https://github.com/programthink/books/blob/master/README.wiki#106-it-公司"), progress: [331, 507], group: "books"},{
@@ -19,7 +24,7 @@ var conf = {
 				"《进阶龙与地下城》,第三章：现实和其他可能性的对比,有趣",
 				"——然而可悲的是……更好的,第四章：有效市场假说,为什么?",
 				"——如果是的话……钱不能吃,,为什么不套利呢？",
-				"A Very Potter Musical,第七章：回赠,"
+				"A Very Potter Musical,第七章：回赠," + "".done.green
 			].map(function (v){return v.split(",")})
 		},
 		progress: [29, 1283]
@@ -47,59 +52,59 @@ var conf = {
 		content: {
 			headers: [["课程", "进度", "状态"]], 
 			body: [
-				["IELTSx - Listening", "Unit Overview".gray, ""],
-				["IELTSx - Speaking".clas("sub ieltsx"), "Unit Overview".gray, ""],
-				["IELTSx - Reading".clas("sub ieltsx"), "Unit Overview".gray, ""],
-				["IELTSx - Writing".clas("sub ieltsx"), "Unit Overview".gray, ""],
-				["IT.1.1x", "What is this course about?".gray, ""],
-				["Think101x", "A taste of things to come".gray, ""],
+	//			["IELTSx - Listening", "Unit Overview".gray, ""],
+	//			["IELTSx - Speaking".clas("sub ieltsx"), "Unit Overview".gray, ""],
+	//			["IELTSx - Reading".clas("sub ieltsx"), "Unit Overview".gray, ""],
+	//			["IELTSx - Writing".clas("sub ieltsx"), "Unit Overview".gray, ""],
+	//			["IT.1.1x", "What is this course about?".gray, ""],
+	//			["Think101x", "A taste of things to come".gray, ""],
 				["Louv1.1x", "Sum of digits with communicating vases".gray, ""],
 				["Louv1.2x", "Motivation for concurrency".green, "".processingL],
 				["CloudIntro.x", "日程管理失败".red, "".processingL],
 				["HTML5.1x", "Course syllabus".green, ""],
-				["HTML5.2x", "".red, ""],
+	//			["HTML5.2x", "".red, ""],
 				["LFS101x.2", "Introduction/ Learning Objectives".gray, ""],
 				["DEV204x", "Introducing C#".gray, ""],
 				["FP101", "Functional Parsers and Monads".green, "".processingL],
-				["PH525.1x", "Getting Started".gray, ""],
+	//			["PH525.1x", "Getting Started".gray, ""],
 				["RTSIx", "Part 1 What are Real-Time Systems".gray, ""],
 				["60240013x", "Homework of Week 3".red + "真的看不懂了……".comment, icon("clear").red],
 				["6.004.1x", "已退选".red + "不明觉厉".comment, icon("clear").red],
 				["6.004.2x", "已退选".red + "……真的。".comment, icon("clear").red],
-				["M101x", "Introduction to the Course", ""],
-				["Code101x", "Pre-Course Survey".gray, ""],
-				["CS50x3", "Important Pre-Course Survey".gray + "我以前上过这课".comment, ""]
+	//			["M101x", "Introduction to the Course", ""],
+	//			["Code101x", "Pre-Course Survey".gray, ""],
+	//			["CS50x3", "Important Pre-Course Survey".gray + "我以前上过这课".comment, ""]
 			]
 		}
 	},{
 		title: "网易云课堂 - CS 大一".link("http://study.163.com/curricula/cs/grade-1.htm"),
 		content: [
-			"程序设计入门 - Python",
+	//		"程序设计入门 - Python",
 			"程序设计入门 - C 语言 > " + " C 语言程序设计进阶".gray,
 			"程序设计入门 - Java > " + " Java 语言程序设计进阶".gray,
-			"计算机专业导论之思维与系统",
-			"计算机专业导论之语言与算法",
-			"计算机专业导论之学科与专业",
+	//		"计算机专业导论之思维与系统",
+	//		"计算机专业导论之语言与算法",
+	//		"计算机专业导论之学科与专业",
 			"工科数学分析(1) > " + " 工科数学分析(2)".gray.processing
 		],
 		progress: [0, 7, 10]
 	},{
-		title: "计算机科学",
-		content: [
-			"计算机体系结构",
-			"计算机网络原理",
-			"数据结构",
-			"算法导论",
-			"计算机组成原理",
-			"图形学",
-			"人工智能",
-			"数据库系统",
-			"编译原理",
-			"操作系统",
-			"网络协议"
-		]
-	}, {
-		title: "数学",
+	//	title: "计算机科学",
+	//	content: [
+	//		"计算机体系结构",
+	//		"计算机网络原理",
+	//		"数据结构",
+	//		"算法导论",
+	//		"计算机组成原理",
+	//		"图形学",
+	//		"人工智能",
+	//		"数据库系统",
+	//		"编译原理",
+	//		"操作系统",
+	//		"网络协议"
+	//	]
+	//}, {
+	//	title: "数学",
 		/*content: [
 			"微积分",
 			"线性代数",
@@ -113,89 +118,90 @@ var conf = {
 			"《什么是数学》",
 			"《数学之美》"
 		]*/
-		content: "还是要学习一个",
+	//	content: "还是要学习一个",
 		//progress: [0, 0, 2]//book only
-	}, {
-		title: "语言",
-		content: [
-			"English".processing.orange,
-			"Esperanto",
-			"Latin"
-		]
-	}, {
+	//}, {
+	//	title: "语言",
+	//	content: [
+	//		"English".processing.orange,
+	//		"Esperanto",
+	//		"Latin"
+	//	]
+	//}, {
 		title: "计算机语言",
 		content: [
 			"JavaScript".done.green,
-			"Python",
+	//		"Python",
 			"Node.js".processing,
-			"C++",
-			"Bash",
+	//		"C++",
+	//		"Bash",
 			"HTML 5 API".processing,
-			"Go",
-			"Dart",
-			"Swift",
+	//		"Go",
+	//		"Dart",
+	//		"Swift",
 			"C#".processing,
-			"Lisp",
+	//		"Lisp",
 			"ECMAScript 2015".processing,
-			"R".processing,
+			"R",
 			"Haskell".processing
 		]
 	}, {
 		title: "思维、逻辑、社会、设计、玄学",
 		content: [
 			"黑客与画家".done.green.id("book_hackers_and_painters"),
-			"批判性思维工具",
+	//		"批判性思维工具",
 			"哥德尔艾舍尔巴赫".processing,
-			"不确定状况下的判断",
-			"影响力:科学与实践",
-			"给大家看的设计书",
-			"可笑的思维谬误：批判性思考与查错神经",
-			"乌合之众",
+	//		"不确定状况下的判断",
+	//		"影响力:科学与实践",
+	//		"给大家看的设计书",
+	//		"可笑的思维谬误：批判性思考与查错神经",
+	//		"乌合之众",
 			"提问的智慧".done.green.id("book_smart_questions"),
-			"浪潮之巅"
+	//		"浪潮之巅"
 		],
-		progress: [2, 3, 10]
+	//	progress: [2, 3, 10]
 	}, {
 		title: "一些奇怪的东西",
 		content: [
 			"魔戒".processing,
-			"DND规则",
-			"1984",
-			"美丽新世界",
+	//		"DND规则",
+	//		"1984",
+	//		"美丽新世界",
 			"三体".done.green
 		]
 	}, {
-		title: "听起来很厉害的样子，说不定会去拜读",
-		content: [
-			"重构",
-			"代码整洁之道",
-			"代码大全2",
-			"数学之美",
-			"Rework中文版",
-			"高效程序员的45个习惯".gray.del,
-			"自私的基因",
-			"失控".processing.green,
-			"万物由来",
-			"黑客攻防技术宝典（Web实战篇）".processing.red,
-			"白帽子讲Web安全",
-			"Web前端黑客技术揭秘",
-			"SQL注入攻击与防御",
-			"JavaScript DOM编程艺术"
-		],
-		progress: [0, 1, 14]
-	}, {
+	//	title: "听起来很厉害的样子，说不定会去拜读",
+	//	content: [
+	//		"重构",
+	//		"代码整洁之道",
+	//		"代码大全2",
+	//		"数学之美",
+	//		"Rework中文版",
+	//		"高效程序员的45个习惯".gray.del,
+	//		"自私的基因",
+	//		"失控".processing.green,
+	//		"万物由来",
+	//		"黑客攻防技术宝典（Web实战篇）".processing.red,
+	//		"白帽子讲Web安全",
+	//		"Web前端黑客技术揭秘",
+	//		"SQL注入攻击与防御",
+	//		"JavaScript DOM编程艺术"
+	//	],
+	//	progress: [0, 1, 14]
+	//}, {
 		title: "Done!",
 		content: {
 			headers: [[]],
 			body: [
-				["提问的智慧".green.link("#book_smart_questions"), "2015/??/??", "".done.green],
-				["黑客与画家".green.link("#book_hackers_and_painters"), "2015/??/??", "".done.green],
-				["Uno", "2015/08/18", "".done.green],
-				["我们的征途，是星辰大海！ - Bilibili 专题".green.link("http://www.bilibili.com/topic/245.html"), "2015/10/4", "".done.green],
-				["平面国", "2015/11/08", "".done.green],
-				["三体", "2015/11/09", "".done.green],
-				["暗时间".green, "2015/12/03", "".done.green]
-			]
+				["提问的智慧".green.link("#book_smart_questions"), "2015/??/??"],
+				["黑客与画家".green.link("#book_hackers_and_painters"), "2015/??/??"],
+				["Uno", "2015/08/18"],
+				["我们的征途，是星辰大海！ - Bilibili 专题".green.link("http://www.bilibili.com/topic/245.html"), "2015/10/4"],
+				["平面国", "2015/11/08"],
+				["三体", "2015/11/09"],
+				["暗时间", "2015/12/03"],
+				["A Very Potter Musical".green.link("http://www.bilibili.com/video/av1409326/"), "2015/12/14"]
+			].map(function (v){v.push("".done.green);return v})
 		}
 	}],
 	motto: "——在一群出色的人中间，常常误以为自己也是其中一员，然后忘了努力"
