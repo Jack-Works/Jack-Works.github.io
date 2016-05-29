cb8c7a50f7ba511a47dbe9b8685c91ffee1144cf
Jack Works <Jack Works>
2016-05-30 02:12:14 +0800

    Change to Vue

--- todo/data.js
+++ todo/data.js
@@ -1,21 +1,23 @@
 var conf = {
 	article_title: 'Todo list of Jack Works',
-	weekMsg: '文明一出天地灭'.replace(/;/g, '<br>'),
-	items: [{
+	weekMsg: '脑袋空空的我'.replace(/;/g, '<br>'),
+	items: [//{
 	//		title: '失控'.link('https://github.com/programthink/books/blob/master/README.wiki#107-其它'), progress: [404, 1027], group: 'books'},{
 	//		title: 'Borland 传奇'.link('https://github.com/programthink/books/blob/master/README.wiki#106-it-公司'), progress: [331, 507], group: 'books'},{
 	//		title: '猜想与反驳'.link('https://github.com/programthink/books/blob/master/README.wiki#88-科学哲学'), progress: [10, 62, 722], group: 'books'},{
 	//		title: 'Doom 启示录', progress: [7, 30, 433], group: 'books'},{
 	//		title: '怎样阅读一本书' + '第五章 如何做一个自我要求的读者'.comment, progress: [17, 114], group: 'books'},
-	}, {
+	//}, 
+	{
 		title: '哥德尔、埃舍尔、巴赫——集异璧之大成' + ' - 重读'.italic + '尚未开始'.comment,
-		group: 'book_review'
+		group: 'book_review',
+		progress: []
 	}, {
-		title: 'Ingress 清明上河图系列任务' + '30 / 654'.comment,
+		title: 'Ingress 清明上河图系列任务',
 		progress: [30, 654]
-	}, ,{
+	},{
 		title: 'Wow! Such plan, so amazing',
-		progress: [15.45, 150000]
+		progress: [15.45, 20655]
 	},  // {
 	//	title: '魔戒',
 	//	content: ['霍比特人'.done.green,
@@ -23,7 +25,7 @@ var conf = {
 	//		'王者归来',
 	//		'精灵宝钻'
 	//	],
-	//	progress: [217, 706, 2004],
+	//	progress: [217, 2004],
 	//	group: 'books_details'
 	//},{
 	//	title: '网易云课堂 - 计算机网络之网尽其用'.link('http://mooc.study.163.com/course/HIT-1000002010?tid=1000003008'),
@@ -33,7 +35,7 @@ var conf = {
 	//		'第 2 章: 计算机网络应用 (上)'.gray,
 	//		'第 2 章: 计算机网络应用 (下)'.gray
 	//	],
-	//	progress: [11, 17, 39],
+	//	progress: [11, 39],
 	//	group: 'netease'
 	//},
 	//{
@@ -76,7 +78,7 @@ var conf = {
 	//		'计算机专业导论之学科与专业',
 	//		'工科数学分析(1) > ' + ' 工科数学分析(2)'.gray.processing
 	//	],
-	//	progress: [0, 7, 10]
+	//	progress: [0, 10]
 	//},{
 	//	title: '计算机科学',
 	//	content: [
@@ -93,22 +95,22 @@ var conf = {
 	//		'网络协议'
 	//	]
 	//}, {
-	//	title: '数学',
-	//	content: [
+	{	title: '数学',
+		content: [
 	//		'微积分',
 	//		'线性代数',
 	//		'数值分析',
 	//		'概率统计',
 	//		'复变函数',
-	//		'高等数学',
-	//		'离散数学'.link('#comb').red,
+			'高等数学',
+			'离散数学'.red,
 	//		'抽象代數',
 	//		'拓扑',
 	//		'《什么是数学》',
 	//		'《数学之美》'
-	//	]
-	//	progress: [0, 0, 2]//book only
-	//}, {
+		],
+		progress: []
+	}, //{
 	//	title: '语言',
 	//	content: [
 	//		'English'.processing.orange,
@@ -119,7 +121,6 @@ var conf = {
 	//}, {
 	//	title: '计算机语言',
 	//	content: [
-	//		'JavaScript'.done.green,
 	//		'Python',
 	//		'Node.js'.processing,
 	//		'C++',
@@ -130,7 +131,6 @@ var conf = {
 	//		'Swift',
 	//		'C#'.processing,
 	//		'Lisp',
-	//		'ECMAScript 2015'.processing,
 	//		'R',
 	//		'Haskell'.processing
 	//	]
@@ -147,9 +147,10 @@ var conf = {
 	//		'可笑的思维谬误：批判性思考与查错神经',
 	//		'乌合之众',
 			'提问的智慧'.done.green.id('book_smart_questions'),
+			'万万没想到——用理工科思维理解世界'.done.green
 	//		'浪潮之巅'
 		],
-	//	progress: [2, 3, 10]
+	//	progress: [2, 10]
 	}, {
 		title: '一些奇怪的东西',
 		content: [
@@ -177,31 +178,33 @@ var conf = {
 	//		'SQL注入攻击与防御',
 	//		'JavaScript DOM编程艺术'
 	//	],
-	//	progress: [0, 1, 14]
+	//	progress: [0, 14]
 	//}, */, {
 		title: '2016年',
 		content: {
-			headers: [[]],
+			headers: [['有趣的事物', '新鲜的评语', '恰好的时间']],
 			body: [
-				['哥德尔、埃舍尔、巴赫——集异璧之大成'.link('#book_geb') + '这句话没有在自指'.comment, '2016/03/06']
-			]
+				['哥德尔、埃舍尔、巴赫——集异璧之大成'.link('#book_geb'), '这句话没有在自指', '2016/03/06'],
+				['万万没想到——用理工科思维理解世界'.link('https://www.amazon.cn/gp/product/B015DLP55A'), '相信我，这真的只是一本博客集', '2016/05/20']
+			].map(v => v.length == 3 ? [v[0], v[1], v[2]] : [v[0], '', v[1]])
 		}
 	}, {
 		title: '2015年',
 		content: {
-			headers: [[]],
+			headers: [['有趣的事物', '新鲜的评语', '恰好的时间']],
 			body: [
-				['提问的智慧'.link('#book_smart_questions') + 'https://google.com/'.comment, '2015/??/??'],
+				['提问的智慧'.link('#book_smart_questions'), 'https://google.com/', '2015/??/??'],
 				['黑客与画家'.link('#book_hackers_and_painters'), '2015/??/??'],
-				['Uno' + '+2 +2 +2 +2 +2!'.comment, '2015/08/18'],
-				['我们的征途，是星辰大海！ - Bilibili 专题'.link('http://www.bilibili.com/topic/245.html'), '2015/10/4'],
+				['Uno', '+2 +2 +2 +2 +2!', '2015/08/18'],
+				['我们的征途，是星辰大海！ - Bilibili 专题'.link('http://www.bilibili.com/topic/245.html'), '2015/10/04'],
 				['平面国', '2015/11/08'],
-				['三体' + '消灭人类暴政，世界属于三体'.comment, '2015/11/09'],
+				['三体', '消灭人类暴政，世界属于三体!!', '2015/11/09'],
 				['暗时间', '2015/12/03'],
-				['哈利波特与理性之道'.link('http://hpmor.lofter.com/') + '严肃地对待荒谬，荒谬地对待严肃'.comment, '2015/12/10'],
-				['A Very Potter Musical'.link('http://www.bilibili.com/video/av1409326/') + '不 我不吃这对 CP'.comment, '2015/12/14'],
-			]//.map(function (v){v.push(''.done.green);return v})
+				['哈利波特与理性之道'.link('http://hpmor.lofter.com/'), '严肃地对待荒谬，荒谬地对待严肃', '2015/12/10'],
+				['A Very Potter Musical'.link('http://www.bilibili.com/video/av1409326/'), 'CP 有毒', '2015/12/14'],
+			].map(v => v.length == 3 ? [v[0], v[1], v[2]] : [v[0], '', v[1]])
 		}
 	}],
 	motto: '——在一群出色的人中间，常常误以为自己也是其中一员，然后忘了努力'
 }
+new Vue({el: 'body', data: conf})
--- todo/types.js
+++ todo/types.js
@@ -1,48 +1,64 @@
 var conf = {article_title: 'Untitled',items: [{title: 'Error'}]}
 
-function table(headers, body, classes){
-	return '<table class="mdl-data-table mdl-shadow--2dp full-width leftAlign ' + (classes || '') + '"><thead>' + 
-		headers.map(function (v){
-			return '<tr>' + v.map(function (t){
-				return '<th>' + t + '</th>'
-			}).join('') + '</tr>'
-		}).join('') + '</thead><tbody>' + 
-		body.map(function (v){
-			return '<tr>' + v.map(function (t){
-				return '<td>' + t + '</td>'
-			}).join('') + '</tr>'
-		}).join('') + '</tbody></table>';
-}
+var table = ({headers, body, classes = ''}) => `
+	<table class="${classes}">
+		<thead>
+			${headers.map(v => `
+				<tr>${v.map(t => `
+					<th>${t}</th>`).join('')}
+				</tr>`).join('')
+			}
+		</thead>
+		
+		<tbody>
+			${body.map(v => `
+				<tr>${v.map(t => `
+					<td>${t}</td>`).join('')}
+				</tr>`).join('')
+			}
+		</tbody>
+	</table>`
 
-function list(data){
-	return '<table class="mdl-data-table mdl-shadow--2dp full-width leftAlign"><tbody>' + 
-		data.map(function (v){
-			return '<tr><td>' + v + '</td></tr>'
-		}).join('') + '</tbody></table>';
-}
-
+var list = data => `<ul class="collection">${data.map(v => `<li class="collection-item">${v}</li>`).join('')}</ul>`
 /* generator name::String param::String||Function => generator
  *           name::Array param::String||Function  => generator(name[x], param)
  */
-(function generator(name, param){
-	if(Array.isArray(name)) {name.forEach(function (value){generator(value, param)})}
-	else if(param instanceof Function) {
-		String.prototype[name] = param;
-	} else {
-		String.prototype.__defineGetter__(name, function () {
-			return param.replace(/Rep/g, name).replace(/This/, this)
-		});
-	}
-	return generator;
-})
-(['red', 'green', 'orange', 'right', 'gray', 'del' ,'italic'], '<span class="Rep">This</span>')
-(['done' ,'clear'], 'This<i class="material-icons right">Rep</i>')
-('processing', 'This<div style="zoom: 0.8;" class="mdl-spinner mdl-js-spinner is-active right"></div>')
-('processingL', 'This<div style="zoom: 0.8;" class="mdl-spinner mdl-js-spinner is-active"></div>')
-('comment', '<span class="gray italic"> - This</span>')
-('id', function (id) {return '<span id="" + id + "">' + this + '</span>'})
-('clas', function (clas) {return '<span class="" + clas + "">' + this + '</span>'})
 
-function icon(icon){
-	return '<i class="material-icons">' + icon + '</i>';
-}
+;(function generator(name, param){
+	if(Array.isArray(name)) name.forEach(v => generator(v, param))
+	else if(param instanceof Function) String.prototype[name] = param
+	else String.prototype.__defineGetter__(name, function () {return param.replace(/Rep/g, name).replace(/This/, this)})
+	return generator
+})
+(['right', 'del' ,'italic'], '<span class="Rep">This</span>')
+(['gray', 'red', 'green', 'orange'], '<span class="s-Rep">This</span>')
+(['done' ,'clear'], 'This<i class="material-icons right">Rep</i>')
+('processing', `This
+	<div class="preloader-wrapper small active" style="zoom: 0.5;float: right;margin-top: 6px;">
+		<div class="spinner-layer spinner-blue-only">
+			<div class="circle-clipper left">
+				<div class="circle"></div>
+			</div>
+			<div class="gap-patch">
+				<div class="circle"></div>
+			</div>
+			<div class="circle-clipper right">
+				<div class="circle"></div>
+			</div>
+		</div>
+	</div>`)
+('processingL', 'This<div style="zoom: 0.8;" class="mdl-spinner mdl-js-spinner is-active"></div>')
+('comment', ' - This'.gray.italic)
+('id', function (id) {return `<span id="${id}">${this}</span>`})
+('clas', function (clas) {return `<span class="${clas}">${this}</span>`})
+var icon = icon => `<i class="material-icons">${icon}</i>`
+
+var typeChecker = (_list, _table) => _list ? list : (_table ? table : raw => raw)
+var drawer = data => typeChecker(Array.isArray(data), data instanceof Object && 'headers' in data && 'body' in data)(data)
+Vue.filter('drawer', drawer)
+Vue.filter('progressHelper', arr => {
+	if(!arr) return ''
+	if(arr.length == 1) return `进度: ${arr[0]}%`.comment
+	if(arr.length == 2) return `进度: ${arr[0]} / ${arr[1]}`.comment
+})
+Vue.filter('progressCalc', arr => 100 * arr[0] / (arr[1] || 100))
