3c0f1b31a19b44a0a597bf1f4c0ba90a66bbeb02
Jack Works <Jack Works>
2016-06-16 15:07:34 +0800

    Tips for non-es6 visitor

--- todo/compile.bat
+++
@@ -1,5 +0,0 @@
-babel --presets=es2015 --out-file=data.min.js data.js
-uglifyjs data.min.js -o data.min.js
-
-babel --presets=es2015 --out-file=types.min.js types.js
-uglifyjs types.min.js -o types.min.js
